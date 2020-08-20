cb62677a74e0e08136ad7d44f46e10179f8d9cf5
austanin <austanin.git@gmail.com>
2020-08-20 19:31:30 +0300

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ docker push austanin/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=austanin/multi-server:$SHA
-kubectl set image deployments/client-deployment server=austanin/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=austanin/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=austanin/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=austanin/multi-worker:$SHA
