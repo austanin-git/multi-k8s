b02ce8dd3a4287bbb4c6ca9a761a8ab972ec677f
austanin <austanin.git@gmail.com>
2020-08-21 14:24:04 +0300

    fix deployment.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 docker build -t austanin/multi-client:latest -t austanin/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t austanin/multi-server:latest -t austanin/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t austanin/multi-worker:latest -t austanin/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push austanin/multi-client:latest
-docker push austanin/multi-server:latest
-docker push austanin/multi-worker:latest
+#docker push austanin/multi-client:latest
+#docker push austanin/multi-server:latest
+#docker push austanin/multi-worker:latest
 
 docker push austanin/multi-client:$SHA
 docker push austanin/multi-server:$SHA
